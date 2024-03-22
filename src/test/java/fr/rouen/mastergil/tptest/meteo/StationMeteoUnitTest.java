ceb1bec8e11d2037686c55f759e40ef968875b59
Erwann27 <montagne.erwann.2@gmail.com>
2024-03-22 14:32:12 +0100

    Added method to test main function

--- src/test/java/fr/rouen/mastergil/tptest/meteo/StationMeteoUnitTest.java
+++ src/test/java/fr/rouen/mastergil/tptest/meteo/StationMeteoUnitTest.java
@@ -3,11 +3,15 @@ package fr.rouen.mastergil.tptest.meteo;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.net.ConnectException;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -19,11 +23,29 @@ class StationMeteoUnitTest {
 
     private IWeatherProvider weatherProvider;
 
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
     @BeforeEach
     public void init() {
         weatherProvider = mock(IWeatherProvider.class);
         when(weatherProvider.getForecastByCity("")).thenReturn(new ArrayList<Prevision>());
     }
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
 
     @Test
     public void ShouldCreateWeatherStation() {
@@ -54,5 +76,16 @@ class StationMeteoUnitTest {
                 // WHEN
                 stationMeteo.majPrevision(city)
         ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void shouldPrintProvision() {
+        // GIVEN
+        String expected = new StationMeteo(new OpenWeatherMapProvider())
+                .majPrevision("Paris,FR").toString().trim();
+        // WHEN
+        StationMeteo.main(null);
+        // THEN
+        assertThat(outContent.toString().trim()).isEqualTo(expected);
     }
 }
